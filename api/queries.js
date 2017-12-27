971e850db783de885e63d5fe78ea10e1139918fc
jeremysandor <jeremy.r.sandor@gmail.com>
2017-12-27 15:37:55 -0800

    Code cleanup

--- api/queries.js
+++ api/queries.js
@@ -38,52 +38,7 @@ class Query {
       res.status(400)
     }
   }  
-
-
+  
 }
 
 module.exports = Query;
-
-// const getAllUsers = async (req, res, next) => {
-//   console.log('DB', db);
-//   try {
-//     const data = await db.any('select * from users');
-//     res.status(200).json(data)
-//   } catch (e) {
-//     console.log('Error:', e)
-//     res.status(400)
-//   }
-  
-// }
-
-// const getUser = async (req, res, next) => {
-//   const {id} = req.params;
-//   const userId = parseInt(id);
-  
-//   try {
-//     const data = await db.one('select * from users where id = $1', userID);
-//     res.status(200).json(data)
-//   } catch (e) {
-//     console.log('Error:', e)
-//     res.status(400)
-//   }
-// }
-
-// const createUser = async (req, res, next) => {
-//   try {
-//     db.none('insert into users(name, age, sex)' + 'values(${name}, ${age}, ${sex})', req.body)
-//     res.status(200).json(data)
-//   } catch (e) {
-//     console.log('Error:', e)
-//     res.status(400)
-//   }
-// }
-
-
-// module.exports = {
-//   getAllUsers: getAllUsers
-//   // getUser: getUser,
-//   // createUser: createUser
-//   // updateUser: updateUser,
-//   // removeUser: removeUser
-// };
