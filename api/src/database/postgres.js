d4c7dc3775efb58a806828f8068b183fbd46085d
jeremysandor <jeremy.r.sandor@gmail.com>
2018-04-25 16:14:20 -0700

    look for database url in env

--- api/src/database/postgres.js
+++ api/src/database/postgres.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')();
-const connectionString = 'postgres://deploy@postgres/slackchamp';
+const connectionString = process.env.DATABASE_URL || 'postgres://deploy@postgres/slackchamp';
 const db = pgp(connectionString);
 
 class Postgres {
